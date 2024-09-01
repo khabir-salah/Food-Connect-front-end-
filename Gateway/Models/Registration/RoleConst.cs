82f3f1e4cdb2fb3bf8d5b41e5e5fc751780ae585
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-01 20:58:35 +0100

    users model added

---
+++ Gateway/Models/Registration/RoleConst.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Constant
+{
+    public  class RoleConst
+    {
+        public const string FamilyHead = "FamilyHead";
+        public const string OrganizationHead = "OrganizationHead";
+        public const string Individual = "Individual";
+        public const string Manager = "Manager";
+        public const string Admin = "Admin";
+    }
+}
---
+++ Gateway/Models/Registration/ViewAllUsersModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.Models.Registration
+{
+    public class AllUsersViewModel
+    {
+        public string Email { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+        public string? Name { get; set; }
+        public bool IsActivated { get; set; }
+        public string Role { get; set; }
+        public Guid UserId { get; set; }
+
+    }
+}

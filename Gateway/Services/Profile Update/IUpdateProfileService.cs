1f111f933e26db73ac2113446f8983c02db32bcf
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-02 22:47:10 +0100

    user profile services feature added

---
+++ Gateway/Services/Profile Update/IUpdateProfileService.cs
@@ -0,0 +1,23 @@
+﻿using Gateway.Models.Donation;
+using Gateway.Models.Registration;
+using Gateway.Models.Update;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.Services
+{
+    public interface IUpdateProfileService
+    {
+        Task<bool> UpdateIndividualProfileAsync(IndividualProfileUpdateModel request, string token);
+        Task<bool> UpdateOrganizationProfileAsync(OrganizationProfileUpdateModel request, string token);
+        Task<bool> UpdateManagerProfileAsync(ManagerProfileUpdateModel request, string token);
+        Task<bool> UpdateFamilyProfileAsync(FamilyUpdateProfileModel request, string token);
+        Task<ViewProfileViewModel> ViewProfileAsync(string token);
+        Task<PaginatedResponse<AllUsersViewModel>> ViewAllUserAsync(string token, int PageNumber = 1, int PageSize = 10);
+        Task<UserDeatilsModel> UserDetailAsync(Guid donationId);
+        Task<bool> ValidateUser(Guid userId);
+    }
+}
---
+++ Gateway/Services/Profile Update/UpdateProfileService.cs
@@ -0,0 +1,101 @@
+﻿using Gateway.Extension;
+using Gateway.Models.Donation;
+using Gateway.Models.Registration;
+using Gateway.Models.Update;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.Services
+{
+    public class UpdateProfileService : IUpdateProfileService
+    {
+        private readonly HttpClient _httpClient;
+        public UpdateProfileService()
+        {
+            _httpClient = new HttpClient
+            {
+                Timeout = TimeSpan.FromDays(7) 
+            };
+
+        }
+
+        public async Task<bool> UpdateIndividualProfileAsync(IndividualProfileUpdateModel request, string token)
+        {
+            var apiUrl = "https://localhost:7005/api/Profile/UpdateProfile-Individual";
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            string json = JsonConvert.SerializeObject(request);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(apiUrl, content);
+            return response.IsSuccessStatusCode? true : false;
+        }
+
+        public async Task<bool> UpdateOrganizationProfileAsync(OrganizationProfileUpdateModel request, string token)
+        {
+            var apiUrl = "https://localhost:7005/api/Profile/UpdateProfile-OrganizationHead";
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            string json = JsonConvert.SerializeObject(request);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(apiUrl, content);
+            return response.IsSuccessStatusCode? true: false;
+        }
+
+        public async Task<bool> UpdateManagerProfileAsync(ManagerProfileUpdateModel request, string token)
+        {
+            var apiUrl = "https://localhost:7005/api/Profile/UpdateProfile-Manager";
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            string json = JsonConvert.SerializeObject(request);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(apiUrl, content);
+            return response.IsSuccessStatusCode ? true : false;
+        }
+
+        public async Task<bool> UpdateFamilyProfileAsync(FamilyUpdateProfileModel request, string token)
+        {
+            var apiUrl = "https://localhost:7005/api/Profile/UpdateProfile-FamilyHead";
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            string json = JsonConvert.SerializeObject(request);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(apiUrl, content);
+            return response.IsSuccessStatusCode ? true : false;
+        }
+
+        public async Task<ViewProfileViewModel> ViewProfileAsync( string token)
+        {
+            var apiUrl = "https://localhost:7005/api/Profile/View-Profile";
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.GetAsync(apiUrl);
+            var result = await response.ReadContentAs<ViewProfileViewModel>();
+            return result;
+        }
+
+        public async Task<PaginatedResponse<AllUsersViewModel>> ViewAllUserAsync(string token, int PageNumber = 1, int PageSize = 10)
+        {
+            var apiUrl = $"https://localhost:7005/api/Profile/ViewAllUsers?PageNumber={PageNumber}&PageSize={PageSize}";
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.GetAsync(apiUrl);
+            var result = await response.ReadContentAs<PaginatedResponse<AllUsersViewModel>>();
+            return result;
+        }
+
+        public async Task<UserDeatilsModel> UserDetailAsync(Guid donationId)
+        {
+            var response = await _httpClient.PutAsync($"https://localHost:7005/api/Profile/details/{donationId}", null);
+            var result = await response.ReadContentAs<UserDeatilsModel>();
+            return result;
+        }
+
+        public async Task<bool> ValidateUser(Guid userId)
+        {
+            var response = await _httpClient.PutAsync($"https://localHost:7005/api/Profile/Validate/{userId}", null);
+            return response.IsSuccessStatusCode ? true : false;
+        }
+    }
+}

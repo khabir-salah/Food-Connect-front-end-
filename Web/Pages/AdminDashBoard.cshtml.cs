da8b1eaa8206b822ce8f1e27f7129cff44b3462f
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-04 13:40:16 +0100

    Admin dashboard view

---
+++ Web/Pages/AdminDashBoard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Shared
+{
+    public class AdminDashBoardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

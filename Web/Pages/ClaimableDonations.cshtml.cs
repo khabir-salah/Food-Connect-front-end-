47dacd9d270119268bf93b478d9494d11a084c3b
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-09-05 17:34:47 +0100

    claimable donation view added

---
+++ Web/Pages/ClaimableDonations.cshtml.cs
@@ -0,0 +1,33 @@
+using Gateway.Models.Donation;
+using Gateway.Services.FoodDonation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages
+{
+    public class ClaimableDonationsModel(IFoodDonationService _foodDonationService) : PageModel
+    {
+        [BindProperty]
+        public PaginatedResponse<UserDonationViewModel> pending { get; set; }
+        public async Task<IActionResult> OnGet()
+        {
+            var token = HttpContext.Session.GetString("JWToken");
+            pending = await _foodDonationService.ReceiveFoodDonation(token);
+            return Page();
+        }
+
+        [BindProperty]
+        public Guid Id { get; set; }
+        public async Task<IActionResult> OnPostClaimAsync()
+        {
+            var token = HttpContext.Session.GetString("JWToken");
+            var claim = await _foodDonationService.ClaimDonation(Id, token);
+            if (!claim)
+            {
+                TempData["Message"] = "You can Only Claim A Donation After 5hrs Of First Claim";
+                return Page();
+            }
+            return RedirectToPage("/SuccessClaimed");
+        }
+    }
+}
